b1e7965d78f7e9a1b4549d86c5757800eb1261ea
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2018-05-09 21:07:37 -0300

    refactored appjs to jsx and added material ui lib

---
+++ src/components/app/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { TextField, FontIcon } from 'material-ui';
+import { blue500 } from 'material-ui/styles/colors';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Movies Now</h1>
+        </header>
+        
+        <MuiThemeProvider>
+          <TextField
+            hintText="Type to search for a movie"
+          />
+          <FontIcon className="material-icons" color={blue500}>search</FontIcon>
+
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default App;
