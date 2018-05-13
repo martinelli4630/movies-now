98cea1575c4189fc9a23e9bc52d72c639bce0901
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2018-05-13 15:11:28 -0300

    fix cardItem css

--- src/views/CardItem.jsx
+++ src/views/CardItem.jsx
@@ -4,19 +4,22 @@ import '../styles/CardItem.scss';
 const baseUrl = "https://image.tmdb.org/t/p/w342";
 
 const CardItem = (obj) => {
-    const { id, title, overview, release_date, vote_average, poster_path  } = obj.item;
+    const { id, title, overview, release_date, vote_average, poster_path } = obj.item;
 
     return (
         <div key={id} className='card-box'>
             <div className='card-details'>
-                <span>{title}</span>
-                <span>Release Date: {release_date}</span>
-                <span>Overview:</span>
-                <span>Ratings: {vote_average}</span>
+                <p className='title'>{title}</p>
+                <span><strong>Release Date:</strong> {release_date}</span>
+                <span className='rate'><strong>Ratings:</strong>
+                    {vote_average}
+                    <i className='material-icons'>star_rate</i>
+                </span>
+                <span><strong>Overview:</strong></span>
                 <span>{overview}</span>
             </div>
             <div className='card-image'>
-                <img src={baseUrl+poster_path} alt={title}/>
+                <img src={baseUrl + poster_path} alt={title} />
             </div>
         </div>
     );
